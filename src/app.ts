0857284feac495c2582c4cf44887213ddebd6b9c
Yugesh428 <yugeshbastola72@gmail.com>
2025-06-23 20:20:16 +0545

    email configured

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import authroute from "./Routes/globals/auth/authRoute";
 import instituteRouter from "./Routes/institueRoute"; // FIX #1 and #2
 import courseRouter from "./Routes/courseRoute";
 import categoryRouter from "./Routes/categoryRoute";
-
+import teacherRouter from "./Routes/teacherRoute"
 const app: Application = express(); // <-- Initialize express app here
 
 app.use(express.json()); // <-- Middleware to parse JSON body
@@ -16,5 +16,6 @@ app.use("/api/auth", authroute);
 app.use("/api/institute", instituteRouter);
 app.use("/api/course", courseRouter);
 app.use("/api/category", categoryRouter);
+app.use("/api/teacher",teacherRouter)
 
 export default app;
